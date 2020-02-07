0aceacbde319238b68b80d68f430defcf0b0342b
PapaCheese <55656526+PapaCheese@users.noreply.github.com>
2020-02-07 22:11:42 +0200

    07/02.2

---
+++ precache-manifest.ceaec448b444f2bdb60cd83ed8db2240.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fed8b47a646dafbc84304aaca7686530",
+    "url": "/guys-site-react/index.html"
+  },
+  {
+    "revision": "d7b7a534560f2d74cccd",
+    "url": "/guys-site-react/static/css/main.c2703356.chunk.css"
+  },
+  {
+    "revision": "2f27ee55f37cd0b8b34e",
+    "url": "/guys-site-react/static/js/2.58a1b85e.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/guys-site-react/static/js/2.58a1b85e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7b7a534560f2d74cccd",
+    "url": "/guys-site-react/static/js/main.327df782.chunk.js"
+  },
+  {
+    "revision": "b57ce208f6501df1ac4a",
+    "url": "/guys-site-react/static/js/runtime-main.b05d42bc.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/guys-site-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.bbe3acaa937b746fefa1c3f8d26d33b4.js"
+  "/guys-site-react/precache-manifest.ceaec448b444f2bdb60cd83ed8db2240.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/guys-site-react/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
