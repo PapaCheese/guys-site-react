c758115097924718d118a7afa04a680fcdb8334e
PapaCheese <55656526+PapaCheese@users.noreply.github.com>
2020-02-07 22:06:20 +0200

    07/02

---
+++ precache-manifest.bbe3acaa937b746fefa1c3f8d26d33b4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e21788153eba35ee279f65285e0fa8b5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "63e160d620b7a461164f",
+    "url": "/static/css/main.c2703356.chunk.css"
+  },
+  {
+    "revision": "2f27ee55f37cd0b8b34e",
+    "url": "/static/js/2.58a1b85e.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.58a1b85e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63e160d620b7a461164f",
+    "url": "/static/js/main.d05a4463.chunk.js"
+  },
+  {
+    "revision": "4fa093af8bad12bd49bb",
+    "url": "/static/js/runtime-main.dccbeb0c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
